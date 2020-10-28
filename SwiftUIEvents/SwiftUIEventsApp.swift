f1d5abcabbb7e3c406de3b2910276464e333f01b
Miyo Alpízar <miyo.naval@gmail.com>
2020-10-28 17:41:50 -0600

    Initial Commit

---
+++ SwiftUIEvents/SwiftUIEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEventsApp.swift
+//  SwiftUIEvents
+//
+//  Created by Miyo Alpízar on 28/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
